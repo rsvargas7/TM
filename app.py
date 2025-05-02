0518892e89f9493cca3e17dd9521e33ac923a7a0
rsvargas7 <rsvargas.742@gmail.com>
2025-05-01 21:04:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ st.write("Versión de Python:", platform.python_version())
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-st.title("Reconocimiento de Imágenes")
+st.title("Identificación de Imágenes")
 #st.write("Versión de Python:", platform.python_version())
-image = Image.open('OIG5.jpg')
+image = Image.open('V.jpg')
 st.image(image, width=350)
 with st.sidebar:
-    st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar")
-img_file_buffer = st.camera_input("Toma una Foto")
+    st.subheader("Con un modelo entrenado en Teachable Machine, puedes aplicarlo en esta app para reconocer")
+img_file_buffer = st.camera_input("Captura una imagen")
 
 if img_file_buffer is not None:
     # To read image file buffer with OpenCV:
